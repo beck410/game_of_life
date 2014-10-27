722d3956482b3ec695f4fdb4ee303cbbfe54d679
beck410 <beck410@hotmail.com>
2014-10-27 16:03:22 -0500

    added random matrixw

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //create initial matrix in array
-var matrix = [ [0, 1, 0], [0, 0, 1], [1, 1, 1] ];
+var matrix = matrixCreator(3,3)
 var $table = document.querySelector("#matrix");
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -11,16 +11,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function conception (matrix) {
   //clear out all state of matrix
   $table.innerHTML = "";
-  //update matrix 
+  //update matrix
   matrix.forEach(function(row){
     //generate rows <tr>
     var $tr = document.createElement("tr");
     $table.appendChild($tr);
     //generate columns <td>
-    row.forEach(function(){
+    row.forEach(function(cell){
       var $td = document.createElement("td");
       $tr.appendChild($td);
+      $td.innerHTML = cell
     });
   })
 }
 
+function matrixCreator(row,col) {
+  var matrix = [];
+  for(var i=0; i<row; i++) {
+    matrix[i] = [];
+    for(var j=0; j<col; j++){
+      matrix[i][j] = Math.round(Math.random())
+    }
+  }
+  return matrix;
+}
